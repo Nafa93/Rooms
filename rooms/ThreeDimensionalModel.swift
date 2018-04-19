a844762d2b739ebc63a5ebfa102a9cfdc21f508e
Nicolas Fernandez Amorosino <>
2018-04-19 14:02:33 -0300

    initial commit to solstice

---
+++ rooms/ThreeDimensionalModel.swift
@@ -0,0 +1,13 @@
+//
+//  3DModel.swift
+//  rooms
+//
+//  Created by Nicolas Fernandez Amorosino on 09/04/2018.
+//  Copyright © 2018 Nicolas Amorosino. All rights reserved.
+//
+
+import Foundation
+
+class 3DModel {
+    
+}
